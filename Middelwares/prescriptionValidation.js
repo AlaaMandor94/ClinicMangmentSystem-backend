33b052583ca2c83eed215b7552d0825b754d338d
mayaahmedabdelsameaa <mayarahmed8624@gmail.com>
2023-04-06 02:34:14 +0200

    update prescription

--- Middelwares/prescriptionValidation.js
+++ Middelwares/prescriptionValidation.js
@@ -1,6 +1,6 @@
 const { body, query, param, validationResult } = require("express-validator");
 let prescriptionValidator = [
-    body("doctorId").isInt().withMessage("doctorid dddd must be number"),
+    body("doctorid").isInt().withMessage("doctorid must be number"),
     body("patientid").isInt().withMessage("patientid must be number"),
     body("clinicid").isInt().withMessage("clinicid must be number"),
     body("medicineid").isArray().withMessage(" medicineid Id must be number"),
